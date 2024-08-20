896dc536bf45902b40dd72bbb2be7618493bb058
yuunaM <the.megacity@icloud.com>
2024-08-20 23:10:55 +0900

    firebase.js 修正

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -12,7 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db };
+export { db };
